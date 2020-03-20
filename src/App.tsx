21656069329dd5134b2756901fbbbe6ea6dc378b
Илья Афонин <OUT-Afonin-II@ca.sbrf.ru>
2020-03-20 18:31:47 +0300

    [CPS-920] Added eslint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { MuiThemeProvider, StylesProvider } from '@material-ui/core';
-import { mainTheme } from "./theme";
-import Home from "./containers/Home";
-import Operations from "./containers/Operations";
-import Header from "./components/Header";
-import { IUserInfo } from "./common/Interfaces/Interfaces";
-import "./App.css";
+import { mainTheme } from './theme';
+import store from './redux/store';
+import Home from './containers/Home';
+import Operations from './containers/Operations';
+import Header from './components/Header';
+import { IUserInfo } from './common/Interfaces/Interfaces';
+import GlobalFonts from './fonts/fonts';
+import './App.css';
 
 const App = (): JSX.Element => {
-  const [value, setValue] = useState < IUserInfo | null>(null);
+  const [value, setValue] = useState <IUserInfo | null>(null);
   useEffect(() => {
     const asyncFetch = async (): Promise<void> => {
       await fetchDataPost();
@@ -56,32 +57,33 @@ const App = (): JSX.Element => {
       credentials: 'include',
     });
     const result = await response.json();
-    sessionStorage.setItem("session_token", result.token);
+    sessionStorage.setItem('session_token', result.token);
     return fetchUserInfo(result.token);
   };
   return (
-  <Provider store={store}>
-    <StylesProvider injectFirst>
-      <MuiThemeProvider theme={mainTheme}>
-        <ThemeProvider theme={mainTheme}>
-          <div className="app">
-            <Router>
-              <Header userInfo={value} />
-              <div className="app-content">
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route exact path="/operations" component={Operations} />
-                  <Route exact path="/directions" render={() => <div>directions</div>} />
-                  <Route exact path="/admin" render={() => <div>admin</div>} />
-                  <Route exact path="*" render={() => <div>Error, page not found!</div>} />
-                </Switch>
-              </div>
-            </Router>
-          </div>
-        </ThemeProvider>
-      </MuiThemeProvider>
-    </StylesProvider>
-  </Provider>
+    <Provider store={store}>
+      <StylesProvider injectFirst>
+        <MuiThemeProvider theme={mainTheme}>
+          <ThemeProvider theme={mainTheme}>
+            <GlobalFonts />
+            <div className="app">
+              <Router>
+                <Header userInfo={value} />
+                <div className="app-content">
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/operations" component={Operations} />
+                    <Route exact path="/directions" render={() => <div>directions</div>} />
+                    <Route exact path="/admin" render={() => <div>admin</div>} />
+                    <Route exact path="*" render={() => <div>Error, page not found!</div>} />
+                  </Switch>
+                </div>
+              </Router>
+            </div>
+          </ThemeProvider>
+        </MuiThemeProvider>
+      </StylesProvider>
+    </Provider>
   );
 };
 
